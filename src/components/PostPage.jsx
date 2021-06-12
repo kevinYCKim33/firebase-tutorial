b4429e8fdb2f3c565a325ac8438614b5d37f5321
kevinYCKim33 <kevinYCKim33@gmail.com>
2021-06-12 13:14:08 -0400

    Finish Routing to Document Data

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -6,13 +6,53 @@ import Comments from "./Comments";
 import { firestore } from "../firebase";
 
 import { collectIdsAndDocs } from "../utilities";
+import { withRouter } from "react-router-dom";
 
 class PostPage extends Component {
   state = { posts: null, comments: [] };
 
+  get postId() {
+    return this.props.match.params.id;
+  }
+
+  get postRef() {
+    return firestore.doc(`posts/${this.postId}`);
+  }
+
+  get commentsRef() {
+    return this.postRef.collection("comments");
+  }
+
+  unsubscribeFromPost = null;
+  unsubscribeFromComments = null;
+
+  componentDidMount = async () => {
+    // pretend millions of people are using it; anytime there're updates to posts and comments this page will auto-update
+    this.unsubscribeFromPost = this.postRef.onSnapshot((snapshot) => {
+      const post = collectIdsAndDocs(snapshot);
+      this.setState({ post });
+    });
+
+    this.unsubscribeFromComments = this.commentsRef.onSnapshot((snapshot) => {
+      const comments = snapshot.docs.map(collectIdsAndDocs);
+      this.setState({ comments });
+    });
+  };
+
+  componentWillUnmount = () => {
+    this.unsubscribeFromPost();
+    this.unsubscribeFromComments();
+  };
+
   render() {
-    return <div>Post Page!</div>;
+    const { post, comments } = this.state;
+    return (
+      <section>
+        {post && <Post {...post} />}
+        <Comments comments={comments} onCreate={() => {}} />
+      </section>
+    );
   }
 }
 
-export default PostPage;
+export default withRouter(PostPage);
