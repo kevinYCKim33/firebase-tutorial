7bda3cfcaacc864b46f46e58acde3b428f4bd648
kevinYCKim33 <kevinYCKim33@gmail.com>
2021-06-13 20:53:08 -0400

    Finish Creating an API Endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,27 @@
 const functions = require("firebase-functions");
-
+const admin = require("firebase-admin");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+
+// Big Idea: run firebase deploy --only functions on terminal,
+// get these cool https urls where you have https endpoints
+
+admin.initializeApp(functions.config().firebase);
+
+const firestore = admin.firestore();
+firestore.settings({ timestampsInSnapshots: true });
+
+// https://us-central1-think-piece-dad44.cloudfunctions.net/helloWorld
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
+});
+
+// https://us-central1-think-piece-dad44.cloudfunctions.net/getAllPosts
+exports.getAllPosts = functions.https.onRequest(async (request, response) => {
+  const snapshot = await firestore.collection("posts").get();
+  const posts = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
+  response.json({ posts });
 });
